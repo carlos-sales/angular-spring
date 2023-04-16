79e8043c6b29792f9fc2506d46a2bc8428b6f762
Carlos Alberto Sales <carlos.gsales27@gmail.com>
2023-04-16 15:27:52 -0300

    HttpClient

--- frontend/src/app/courses/courses/courses.component.ts
+++ frontend/src/app/courses/courses/courses.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -9,8 +10,8 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CoursesComponent
 {
-  public  courses : Course[] = [];
-  public  displayedColumns  = [ 'name', 'category' ];
+  public  courses : Observable<Course[]>;
+  public  displayedColumns    = [ 'name', 'category' ];
 
   constructor( private coursesService: CoursesService )
   {
--- frontend/src/app/courses/services/courses.service.ts
+++ frontend/src/app/courses/services/courses.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { first, tap } from 'rxjs';
 
 import { Course } from '../model/course';
 
@@ -8,13 +9,16 @@ import { Course } from '../model/course';
 })
 export class CoursesService
 {
+  private readonly API = '/assets/courses.json';
 
   constructor( private httpClient: HttpClient ) { }
 
-  list() : Course[]
+  list()
   {
-    return  [
-              { _id: '1', name: 'Angular', category: 'front-end' }
-            ];
+    return  this.httpClient.get<Course[]>( this.API )
+            .pipe(
+              first(),
+              tap( courses => console.log( courses ) )
+            );
   }
 }
