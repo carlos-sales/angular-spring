033aa2e96c19db955328955f44cdc6ce50e3965d
Carlos Alberto Sales <carlos.gsales27@gmail.com>
2023-04-20 14:09:37 -0300

    Base de Dados - primeiro commit

--- backend/src/main/java/com/carlos/backend/repository/CourseRepository.java
+++ backend/src/main/java/com/carlos/backend/repository/CourseRepository.java
@@ -1,11 +1,9 @@
 package com.carlos.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.carlos.backend.model.Course;
 
-@Repository
 public interface CourseRepository extends JpaRepository<Course, Long>
 {
     
