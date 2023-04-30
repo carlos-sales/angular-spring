4722a2f8102ee67303f838124916cb687866d97b
Carlos Alberto Sales <carlos.gsales27@gmail.com>
2023-04-29 22:14:30 -0300

    Atualizar curso: Sprint HTTP Put

--- backend/src/main/java/com/carlos/backend/controller/CourseController.java
+++ backend/src/main/java/com/carlos/backend/controller/CourseController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,18 +35,31 @@ public class CourseController
     public ResponseEntity<Course> findById( @PathVariable Long id )
     {
         return courseRepository.findById( id )
-            .map( record -> ResponseEntity.ok().body(record) )
+            .map( recordFound -> ResponseEntity.ok().body(recordFound) )
             .orElse( ResponseEntity.notFound().build() );
     }
 
     // @RequestMapping( method = RequestMethod.POST )
     @PostMapping
-    public ResponseEntity<Course> create( @RequestBody Course record )
+    public ResponseEntity<Course> create( @RequestBody Course course )
     {
         // System.out.println( record.getName() );
         // return courseRepository.save( record );
         return ResponseEntity
             .status( HttpStatus.CREATED )
-            .body( courseRepository.save( record ) );
+            .body( courseRepository.save( course ) );
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update( @PathVariable Long id, @RequestBody Course course )
+    {
+        return courseRepository.findById( id )
+            .map( recordFound -> {
+                recordFound.setName(course.getName());
+                recordFound.setCategory(course.getCategory());
+                Course updated = courseRepository.save(recordFound);
+                return ResponseEntity.ok().body(updated);
+            })
+            .orElse( ResponseEntity.notFound().build() );
     }
 }
--- frontend/src/app/courses/services/courses.service.ts
+++ frontend/src/app/courses/services/courses.service.ts
@@ -19,17 +19,31 @@ export class CoursesService
             .pipe(
               first(),
               // delay(5000),
-              tap( courses => console.log( courses ) )
+              // tap( courses => console.log( courses ) )
             );
-  }
-
-  save( record: Partial<Course> )
-  {
-    return this.httpClient.post<Course>( this.API, record ).pipe( first() );
   }
 
   loadById( id: string )
   {
     return this.httpClient.get<Course>( `${this.API}/${id}` );
+  }
+
+  save( record: Partial<Course> )
+  {
+    if( record._id )
+    {
+      return this.update( record );
+    }
+    return this.create( record );
+  }
+
+  private create( record: Partial<Course> )
+  {
+    return this.httpClient.post<Course>( this.API, record ).pipe( first() );
+  }
+
+  private update( record: Partial<Course> )
+  {
+    return this.httpClient.put<Course>( `${this.API}/${record._id}`, record ).pipe( first() );
   }
 }
