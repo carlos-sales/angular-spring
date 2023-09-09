c658baf6f690de56ca54b618630bb87e9d5c2f01
Carlos Alberto Sales <carlos.gsales27@gmail.com>
2023-09-09 16:09:19 -0300

    Classe utilitária para validações no form

--- frontend/src/app/shared/form/form-utils.service.ts
+++ frontend/src/app/shared/form/form-utils.service.ts
@@ -8,6 +8,22 @@ export class FormUtilsService {
 
   constructor() { }
 
+  validateAllFormFields( formGroup: UntypedFormGroup | UntypedFormArray )
+  {
+    Object.keys( formGroup.controls ).forEach( field => {
+      const control = formGroup.get( field );
+      if( control instanceof UntypedFormControl )
+      {
+        control.markAsTouched({onlySelf: true});
+      }
+      else if( control instanceof UntypedFormGroup || control instanceof UntypedFormArray )
+      {
+        control.markAsTouched({onlySelf: true});
+        this.validateAllFormFields(control);
+      }
+    })
+  }
+
   getErrorMessage( formGroup: UntypedFormGroup, fieldName: string )
   {
     const field = formGroup.get( fieldName ) as UntypedFormControl;
